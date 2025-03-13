8cf7a3c8f29e5476cf90fd87f5e71b1cc5e3a310
naresh <Naresh@gmail.com>
2025-03-13 11:14:56 +0530

    auto

--- 02-mysql.sh
+++ 02-mysql.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 COMPONENT=mysql
 LOGFILE=/tmp/$COMPONENT.log
-if [ $(id -u root) -ne 0 ];
+if [ $(id -u) -ne 0 ];
 then
-echo -n "\e[31m Your NOT a root user, Please use sudo \e[0m"
+echo -e "\e[31m You do not have root privileges, Please use sudo \e[0m"
 exit 2
 else
-echo -n "\e[32mYour running as admin, Proceeding for next steps\e[0m"
+echo -e "\e[32mYour running as admin, Proceeding for next steps\e[0m"
 fi
 
 
@@ -21,15 +21,15 @@ fi
 
 read -p "Enter mysql password :" MYSQLPASSWORD
 
-echo -e "$COMPONENT is instaling :"
+echo -n "$COMPONENT is instaling :"
 dnf install mysql-server -y &>> $LOGFILE
 stat $?
 
-echo -e "$COMPONENT is starting :"
+echo -n "$COMPONENT is starting :"
 systemctl enable mysqld
 systemctl start  mysqld
 stat $?         
 
-echo -e "$COMPONENT setting root password :"
+echo -n "$COMPONENT setting root password :"
 mysql_secure_installation --set-root-pass $MYSQLPASSWORD
 stat $?
