997e6a974382bd5c2fd02fc060f67dd388cc3a53
naresh <Naresh@gmail.com>
2025-03-13 12:44:50 +0530

    auto

--- 01-frontend.sh
+++ 01-frontend.sh
@@ -2,6 +2,15 @@
 
 COMPONENT=nginx
 LOGFILE=/tmp/$COMPONENT.log
+
+if [ $(id -u) -ne 0 ];
+then
+echo -e "\e[31m Your NOT a root user, Please use sudo \e[0m"
+exit 2
+else
+echo -e "\e[32m Your running as admin, Proceeding for next steps\e[0m"
+fi
+
 stat() {
 if [ $1 -eq 0 ];
 then
